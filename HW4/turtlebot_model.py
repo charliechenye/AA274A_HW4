6dbb1bee9c3181c739156598908a50c30af01513
Chenye Zhu <zhuchenye@gmail.com>
2021-11-24 05:01:22 -0800

    Revert wrap around for alpha

--- HW4/turtlebot_model.py
+++ HW4/turtlebot_model.py
@@ -108,7 +108,7 @@ def transform_line_to_scanner_frame(line, x, tf_base_to_camera, compute_jacobian
     x_cam, y_cam, th_cam = x + np.dot(rotation_matrix, tf_base_to_camera)
     # line parameter in camera frame, new alpha and new r
     r_c = r - x_cam * np.cos(alpha) - y_cam * np.sin(alpha)
-    h = np.array([(alpha - th_cam + np.pi) % (2 * np.pi) - np.pi,
+    h = np.array([alpha - th_cam,
                   r - x_cam * np.cos(alpha) - y_cam * np.sin(alpha)])
 
     if compute_jacobian:
