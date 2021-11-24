77a5d35a7d6cf3c1cac37dfb04b6dab7749f37e8
Chenye Zhu <zhuchenye@gmail.com>
2021-11-24 04:40:45 -0800

    Wrap around for alpha

--- HW4/turtlebot_model.py
+++ HW4/turtlebot_model.py
@@ -107,7 +107,7 @@ def transform_line_to_scanner_frame(line, x, tf_base_to_camera, compute_jacobian
     # pose of camera in world frame
     x_cam, y_cam, th_cam = x + np.dot(rotation_matrix, tf_base_to_camera)
     # line parameter in camera frame, new alpha and new r
-    h = np.array([alpha - th_cam,
+    h = np.array([(alpha - th_cam + np.pi) % (2 * np.pi) - np.pi,
                   r - x_cam * np.cos(alpha) - y_cam * np.sin(alpha)])
 
     if compute_jacobian:
